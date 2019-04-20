15222dc43d6aa8c1dc50abf3b69d51c6d87c8236
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-04-20 20:16:18 -0300

    3 files modified

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+document.getElementById('title').onclick = function () {
+  alert("hello, again");
+}
